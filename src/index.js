48fbc387a3763879d47e31a50fd56507de66ae15
Yvan Yang <yvan.yang@ulsee.com>
2021-11-06 10:37:27 +0800

    update config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 async function getComponent() {
   const element = document.createElement('div');
-  const { default: _ } = await import('./helper');
+  const { default: _ } = await import('lodash');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  // devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
   optimization: {
     runtimeChunk: 'single',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Code Split',
+    }),
+  ],
 };
